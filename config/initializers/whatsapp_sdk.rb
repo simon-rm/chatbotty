0004aaf7b5cd69e7a7586cedca5321830341e20e
simon <simonrmurcia@gmail.com>
2025-04-03 12:45:55 +0000

    bugfix: master key doesnt seem to be available during initialization in prod..

--- config/initializers/whatsapp_sdk.rb
+++ config/initializers/whatsapp_sdk.rb
@@ -1,5 +1,5 @@
 WhatsappSdk.configure do |config|
-  config.access_token = Rails.application.credentials.whatsapp.access_token
+  config.access_token = Rails.application.credentials.dig(:whatsapp, :access_token)
   config.logger = Logger.new(STDOUT) # optional, Faraday logger to attach
   config.logger_options = { bodies: true } # optional, they are all valid logger_options for Faraday
 end
