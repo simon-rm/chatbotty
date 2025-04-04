ec33e7e5f575a051eb5a10dc284190d7e77c1da1
simon <simonrmurcia@gmail.com>
2025-04-04 16:14:03 +0000

    realized wa_id is the phone number, and not another kind of identifier. Modified code accordingly

--- app/controllers/webhooks/whatsapp_controller.rb
+++ app/controllers/webhooks/whatsapp_controller.rb
@@ -1,6 +1,6 @@
 class Webhooks::WhatsappController < ApplicationController
   def respond
-    user = User.find_or_create_by(wa_id: user_attributes[:wa_id])
+    user = User.find_or_create_by(phone_number: user_attributes[:phone_number])
     user.update(user_attributes)
     Rails.logger.info(user_attributes.merge(message_attributes).to_s)
     LLM::WhatsappChat.call(message_attributes:, user:)
@@ -11,8 +11,7 @@ class Webhooks::WhatsappController < ApplicationController
   def user_attributes
     {
       name: whatsapp_attributes.dig("contacts", 0, "profile", "name"),
-      phone_number: whatsapp_attributes.dig("metadata", "display_phone_number"),
-      wa_id: whatsapp_attributes.dig("contacts", 0, "wa_id")
+      phone_number: whatsapp_attributes.dig("contacts", 0, "wa_id")
     }
   end
 
