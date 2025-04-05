9c4b35758c357c2eefee387a9d2a09e282c50c9f
simon <simonrmurcia@gmail.com>
2025-04-05 13:28:26 +0000

    bugfix: only call LLM::WhatsappChat if there is an incoming message (as opposed to a message status update)

--- app/controllers/webhooks/whatsapp_controller.rb
+++ app/controllers/webhooks/whatsapp_controller.rb
@@ -1,29 +1,36 @@
 class Webhooks::WhatsappController < ApplicationController
   def respond
-    user = User.find_or_create_by(phone_number: user_attributes[:phone_number])
-    user.update(user_attributes)
-    Rails.logger.info(user_attributes.merge(message_attributes).to_s)
-    LLM::WhatsappChat.call(message_attributes:, user:)
+    if whatsapp_params["messages"]
+      user = User.find_or_create_by(phone_number: user_attributes[:phone_number])
+      user.update(user_attributes)
+      Rails.logger.info(user_attributes.merge(message_attributes).to_s)
+      result = LLM::WhatsappChat.call(message_attributes:, user:)
+      if result.success?
+        head :ok
+      else
+        render json: { error: result.error }, status: :internal_server_error # generic error
+      end
+    end
   end
 
   private
 
   def user_attributes
     {
-      name: whatsapp_attributes.dig("contacts", 0, "profile", "name"),
-      phone_number: whatsapp_attributes.dig("contacts", 0, "wa_id")
+      name: whatsapp_params.dig("contacts", 0, "profile", "name"),
+      phone_number: whatsapp_params.dig("contacts", 0, "wa_id")
     }
   end
 
   def message_attributes
     {
-      text: whatsapp_attributes.dig("messages", 0, "text", "body"),
-      mid: whatsapp_attributes.dig("messages", 0, "id"),
+      text: whatsapp_params.dig("messages", 0, "text", "body"),
+      mid: whatsapp_params.dig("messages", 0, "id"),
       platform: :whatsapp
     }
   end
 
-  def whatsapp_attributes
+  def whatsapp_params
     params.dig("entry", 0, "changes", 0, "value")
   end
 end
